631411fbdd3f1f1e331e592ff03deca99c00ef06
Rand01ph <361212419@qq.com>
2015-03-08 11:14:53 +0800

    change wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,16 @@
 from __future__ import unicode_literals
 
 import os
+import sys
+import site
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_DIRNAME = PROJECT_ROOT.split(os.sep)[-1]
+
+site.addsitedir('/home/tan/.virtualenvs/md/lib/python2.7/site-packages')
+
+sys.path.append(os.path.join(PROJECT_ROOT, ".."))
+
 settings_module = "%s.settings" % PROJECT_ROOT.split(os.sep)[-1]
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", settings_module)
 
