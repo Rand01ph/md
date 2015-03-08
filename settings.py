bff23df1b2113caf66c3a733fce9a0ef487302ed
Rand01ph <361212419@qq.com>
2015-03-07 21:01:59 -0500

    首页修改

--- settings.py
+++ settings.py
@@ -167,7 +167,7 @@ DATABASES = {
         # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
         "ENGINE": "django.db.backends.mysql",
         # DB name or path to database file if using sqlite3.
-        "NAME": "",
+        "NAME": "tantest01",
         # Not used with sqlite3.
         "USER": "root",
         # Not used with sqlite3.
